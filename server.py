91033c1c7e66cce4cbc5100d3181a4635ef65a1f
gesifeta <adamgemechu@gmail.com>
2025-07-19 11:52:09 +0300

    added dynamic templating using open api genderize

--- server.py
+++ server.py
@@ -1,12 +1,20 @@
 from flask import Flask, render_template
+import requests
 import datetime
 
 app = Flask(__name__)
+
 
 @app.route('/')
 def index():
     year = datetime.date.today().year
     return render_template("index.html",year=year)
-
+@app.route('/guess/<name>')
+def about(name):
+    response = requests.get(f"http://api.genderize.io/?name={name}")
+    response.raise_for_status()
+    data = response.json()
+    year = datetime.date.today().year
+    return render_template("index.html",year=year,gender=data["gender"], name=data["name"],probability=data["probability"])
 if __name__ == '__main__':
     app.run(debug=True)
